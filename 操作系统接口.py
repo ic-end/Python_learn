71145b04f2496cac323b317ce8445f37ce3c03c3
muzichao <568191222@qq.com>
2014-07-19 21:28:54 +0800

    操作系统接口

---
+++ 操作系统接口.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        Python标准库
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     19/02/2014
+# Copyright:   (c) Administrator 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import os
+print(os.getcwd())
+#print(dir(os))
