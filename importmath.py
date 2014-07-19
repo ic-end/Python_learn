f9498a94572321656598ec369c26555186ca9100
muzichao <568191222@qq.com>
2014-07-19 21:27:23 +0800

    import模块

---
+++ importmath.py
@@ -0,0 +1,12 @@
+#import math
+#print math.sqrt(25)
+
+#from math import sqrt
+#print sqrt(25)
+
+#from math import*
+#print sqrt(25)
+
+import math            
+everything = dir(math)  
+print everything       
