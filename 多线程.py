80c7c0908407e1cb40da0d58f9d132a1fd558a99
muzichao <568191222@qq.com>
2014-07-19 21:28:48 +0800

    多线程

---
+++ 多线程.py
@@ -0,0 +1,30 @@
+#-------------------------------------------------------------------------------
+# Name:        多线程
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     19/02/2014
+# Copyright:   (c) Administrator 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import threading, zipfile
+
+class AsyncZip(threading.Thread):
+    def __init__(self, infile, outfile):
+        threading.Thread.__init__(self)
+        self.infile = infile
+        self.outfile = outfile
+    def run(self):
+        f = zipfile.ZipFile(self.outfile, 'w', zipfile.ZIP_DEFLATED)
+        f.write(self.infile)
+        f.close()
+        print('Finished background zip of:', self.infile)
+
+background = AsyncZip('mydata.txt', 'myarchive.zip')
+background.start()
+print('The main program continues to run in foreground.')
+
+background.join()    # Wait for the background task to finish
+print('Main program waited until background was done.')
