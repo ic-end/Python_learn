3d1774298368ba80cae39ca89708b4681709cd91
muzichao <568191222@qq.com>
2014-07-19 21:26:25 +0800

    list列表

---
+++ list.py
@@ -0,0 +1,42 @@
+"""
+suitcase = [] 
+suitcase.append("sunglasses")
+
+suitcase.append("shirt")
+suitcase.append("black")
+suitcase.append("trouser")
+
+list_length = len(suitcase)# Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
+
+"""
+
+"""
+numlist = ["num1", "num2", "num3", "num4", "num5"]
+print numlist
+
+numlist.reverse() 
+print numlist
+
+numlist.sort()
+print numlist
+
+numlist.append("num7")
+print numlist
+
+numlist2 = ["num8", "num9"]
+numlist.extend(numlist2)
+print numlist
+
+numlist.insert(5, "num6")
+print numlist
+
+"""
+
+mixlist = ["num1", "num2", 100, 200]
+print mixlist
+print mixlist[1]
+print mixlist[2]
+print mixlist[1:3]
